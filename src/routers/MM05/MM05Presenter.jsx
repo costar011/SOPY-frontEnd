9e0aeaf128a105a1556849863e92eb3259c26c71
YERIM <dnaer456@gmail.com>
2020-12-19 17:10:36 +0900

    user

--- src/routers/MM05/MM05Presenter.jsx
+++ src/routers/MM05/MM05Presenter.jsx
@@ -1,4 +1,15 @@
 import React from "react";
+import styled from "styled-components";
+
+const wrapper = styled.div`
+  width: 100%;
+  padding: 40px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 const MM05Presenter = () => {
   return `Hello MM05`;
