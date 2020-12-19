7a34c311d9a4d0b10b2934d386ac8a79e238b781
YERIM <dnaer456@gmail.com>
2020-12-19 18:39:22 +0900

    MM05

--- src/routers/MM05/MM05Container.jsx
+++ src/routers/MM05/MM05Container.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import useInput from "../../Hooks/useinput";
 import MM05Presenter from "./MM05Presenter";
 
 const MM05Container = () => {
   ////////   VARIABLE     ////////
 
   ////////   USE STATE    ////////
+  const newName = useInput("");
+  const newEmail = useInput("");
+  const newMobile = useInput("");
+  const newNickName = useInput("");
+  const newZoneCode = useInput("");
+  const newAddress = useInput("");
+  const newDetailAddress = useInput("");
 
   ////////   USE REF      ////////
 
@@ -16,6 +24,16 @@ const MM05Container = () => {
 
   ////////   USE EFFECT   ////////
 
-  return <MM05Presenter />;
+  return (
+    <MM05Presenter
+      newName={newName}
+      newEmail={newEmail}
+      newMobile={newMobile}
+      newNickName={newNickName}
+      newZoneCode={newZoneCode}
+      newAddress={newAddress}
+      newDetailAddress={newDetailAddress}
+    />
+  );
 };
 export default MM05Container;
--- src/routers/MM05/MM05Presenter.jsx
+++ src/routers/MM05/MM05Presenter.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Typist from "react-typist";
+import Fade from "react-reveal/Fade";
 
 const Wrapper = styled.div`
   width: 100%;
@@ -12,14 +13,24 @@ const Wrapper = styled.div`
   justify-content: center;
 `;
 
-const TextInput = styled.div`
-  width: 450px;
+const RowWrapper = styled.div`
+  width: 100%;
+  padding: 40px;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+const TextInput = styled.input`
+  width: ${(props) => props.width || `450px`};
   height: 35px;
   border-radius: 10px;
   margin: 10px 0px;
-  padding: 0px 15px;
+  padding: 0px 10px;
   outline: none;
-  border: 2px solid ${(props) => props.theme.greyColor};
+  border: 1px solid ${(props) => props.theme.greyColor};
   background: none;
   box-shadow: ${(props) => props.theme.boxShadow};
   transition: 0.5s;
@@ -34,15 +45,15 @@ const TextInput = styled.div`
 `;
 
 const Title = styled.h2`
-  font-size: 27px;
-  font-weight: 700px;
+  font-size: 26px;
+  font-weight: 700;
   margin: 0px 0px 40px 0px;
 `;
 
 const Button = styled.button`
   width: 250px;
   height: 30px;
-  border-radius: ${(props) => props.theme.radios};
+  border-radius: ${(props) => props.theme.radius};
   outline: none;
   border: none;
   background-color: ${(props) => props.theme.checkColor};
@@ -54,12 +65,32 @@ const Button = styled.button`
 
   &:hover {
     background-color: ${(props) => props.theme.whiteColor};
-    border: 2px solid ${(props) => props.theme.checkColor};
+    border: 1px solid ${(props) => props.theme.checkColor};
     color: ${(props) => props.theme.checkColor};
   }
 `;
 
-const MM05Presenter = () => {
+const PostButton = styled.button`
+  width: 140px;
+  height: 30px;
+  border-radius: ${(props) => props.theme.radius};
+  outline: none;
+  border: none;
+  background-color: ${(props) => props.theme.pointColor};
+  color: ${(props) => props.theme.whiteColor};
+  cursor: pointer;
+  margin-left: 10px;
+
+  transition: 0.5s;
+
+  &:hover {
+    background-color: ${(props) => props.theme.whiteColor};
+    border: 1px solid ${(props) => props.theme.pointColor};
+    color: ${(props) => props.theme.pointColor};
+  }
+`;
+
+const MM05Presenter = ({ newName }) => {
   return (
     <Wrapper>
       <Typist
@@ -67,16 +98,37 @@ const MM05Presenter = () => {
           show: false,
         }}
       >
-        <Title>SING UP</Title>
+        <Title>SIGN UP</Title>
       </Typist>
+      <Fade bottom>
+        <TextInput type="text" placeholder={`EMAIL…`} {...newEmail} />
+        <TextInput type="text" placeholder={`NAME…`} {...newName} />
+        <TextInput type="text" placeholder={`NICKNAME…`} {...newNickName} />
+        <TextInput type="text" placeholder={`MOBILE…`} {...newMobile} />
+        <RowWrapper>
+          <TextInput
+            type="text"
+            width={`300px`}
+            placeholder={`30560`}
+            readOnly={true}
+            {...newZoneCode}
+          />
+          <PostButton>검색</PostButton>
+        </RowWrapper>
+        <TextInput
+          type="text"
+          placeholder={`ADDRESS…`}
+          readOnly={true}
+          {...newAddress}
+        />
+        <TextInput
+          type="text"
+          placeholder={`DETAILADDRESS…`}
+          {...newDetailAddress}
+        />
 
-      <TextInput type="text" />
-      <TextInput type="text" />
-      <TextInput type="text" />
-      <TextInput type="text" />
-      <TextInput type="text" />
-      <TextInput type="text" />
-      <TextInput type="text" />
+        <Button>SIGN UP !</Button>
+      </Fade>
     </Wrapper>
   );
 };
