0b5a970f44e6efd8cace5b08d052400a66a21cbc
YERIM <dnaer456@gmail.com>
2021-01-02 18:54:12 +0900

    log in

--- src/routers/MM04/MM04Container.jsx
+++ src/routers/MM04/MM04Container.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import MM04Presenter from "./MM04Presenter";
+import useInput from "../../Hooks/useinput";
+import { TRY_LOGIN } from "./MM04Querise";
+import { useMutation } from "react-apollo-hooks";
 
 const MM04Container = () => {
   ////////   VARIABLE     ////////
 
   ////////   USE STATE    ////////
+  const inputEmail = useInput(``);
 
   ////////   USE REF      ////////
 
@@ -13,9 +17,24 @@ const MM04Container = () => {
   ////////   USE QUREY    ////////
 
   ////////   USE MUTATION ////////
+  const [tryLoginMutation] = useMutation(TRY_LOGIN);
 
   ////////   USE EFFECT   ////////
+  const loginClickHandler = async () => {
+    const { data } = await tryLoginMutation({
+      variables: {
+        email: inputEmail.value,
+      },
+    });
 
-  return <MM04Presenter />;
+    console.log(data);
+  };
+
+  return (
+    <MM04Presenter
+      inputEmail={inputEmail}
+      loginClickHandler={loginClickHandler}
+    />
+  );
 };
 export default MM04Container;
