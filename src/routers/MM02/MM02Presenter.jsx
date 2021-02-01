f4a447ff40e94bb68037b05e016f886beae8298c
YERIM <dnaer456@gmail.com>
2021-02-01 23:31:17 +0900

    MM02

--- src/routers/MM02/MM02Presenter.jsx
+++ src/routers/MM02/MM02Presenter.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import {
+  Wrapper,
+  ImageBox,
+  CommonBtn,
+} from "../../components/commonComponents";
+import Fade from "react-reveal/Fade";
+import styled from "styled-components";
+
+const BarWrapper = styled.div`
+  width: 1350px;
+  height: 35px;
+  border-radius: 8px;
+  background-color: ${(props) => props.theme.subThemeColor};
+  box-shadow: ${(props) => props.theme.boxShadow};
+  margin: 30px 0px;
+`;
 
 const MM02Presenter = () => {
-  return `Hello MM02`;
+  return (
+    <Wrapper>
+      <Fade bottom>
+        <Wrapper>Video Upload</Wrapper>
+      </Fade>
+    </Wrapper>
+  );
 };
 
 export default MM02Presenter;
