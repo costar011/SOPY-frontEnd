60eaa94fbb0de4a506782a7a35fd1d27890e4d90
YERIM <dnaer456@gmail.com>
2021-02-02 20:12:33 +0900

    MM02

--- src/routers/MM02/MM02Presenter.jsx
+++ src/routers/MM02/MM02Presenter.jsx
@@ -1,13 +1,57 @@
 import React from "react";
-import {
-  Wrapper,
-  ImageBox,
-  CommonBtn,
-} from "../../components/commonComponents";
+import { Wrapper } from "../../components/commonComponents";
 import Fade from "react-reveal/Fade";
 import styled from "styled-components";
 
-const MM02Presenter = () => {
+const FileInput = styled.input`
+  display: none;
+`;
+
+const FileLabel = styled.label`
+  width: 120px;
+  height: 25px;
+  line-height: 25px;
+  background-color: ${(props) => props.theme.checkColor};
+  color: #fff;
+  box-shadow: ${(props) => props.theme.boxShadow};
+  margin: 5px;
+  border-radius: ${(props) => props.theme.radius};
+  text-align: center;
+  cursor: pointer;
+`;
+
+const TextInput = styled.input`
+  width: 640px;
+  height: 30px;
+  border-radius: 4px;
+  outline: none;
+  border: 1px solid #ccc;
+  margin: 10px 0px;
+  transition: 0.5s;
+  padding: 0px 10px;
+
+  &:focus {
+    box-shadow: 2px 3px 2px #999;
+  }
+`;
+
+const TextArea = styled.textarea`
+  width: 640px;
+  height: 300px;
+  border-radius: 4px;
+  outline: none;
+  border: 1px solid #ccc;
+  margin: 10px 0px;
+  transition: 0.5s;
+  padding: 10px;
+  resize: none;
+
+  &:focus {
+    box-shadow: 2px 3px 2px #999;
+  }
+`;
+
+const MM02Presenter = (fileChangeHandler) => {
   return (
     <Wrapper>
       <Fade bottom>
@@ -15,14 +59,12 @@ const MM02Presenter = () => {
           Video Upload
         </Wrapper>
       </Fade>
-      <Fade>
-        <ImageBox
-          width={`400px`}
-          height={`200px`}
-          margin={`8px`}
-          bgImg={`https://images.unsplash.com/photo-1580927752452-89d86da3fa0a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80`}
-        />
-      </Fade>
+      <FileInput type="file" id="file-js" onChange={fileChangeHandler} />
+
+      <TextInput readOnly={true} />
+      <TextArea readOnly={true} />
+
+      <FileLabel htmlFor="file-js">썸네일</FileLabel>
     </Wrapper>
   );
 };
