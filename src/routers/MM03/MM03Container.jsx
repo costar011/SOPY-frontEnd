f493d35f8bb3c76b6036d270604713426114ed6f
YERIM <dnaer456@gmail.com>
2021-02-09 22:39:59 +0900

    Edit video

--- src/routers/MM03/MM03Container.jsx
+++ src/routers/MM03/MM03Container.jsx
@@ -1,16 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import { useQuery } from "react-apollo-hooks";
+import useInput from "../../Hooks/useInput";
 import MM03Presenter from "./MM03Presenter";
+import { GET_VIDEO_ONE } from "./MM03Queries";
 
-const MM03Container = () => {
+const MM03Container = ({ match }) => {
   //////////// VARIABLE  ////////////////////
+  const videoId = match.params.id;
+
+  console.log(videoId);
+
   //////////// USE STATE  ///////////////////
+  const editThumbnail = useInput(``);
+  const editTitle = useInput(``);
+  const editDesc = useInput(``);
+
+  const [dataFlag, setDataFlag] = useState(true);
+
   //////////// USE REF  /////////////////////
+
   //////////// USE CONTEXT  /////////////////
+
   ////////////// USE QUERY  /////////////////
+  const { data: vData, loading: vLoading, refetch: vRefetch } = useQuery(
+    GET_VIDEO_ONE,
+    {
+      variables: {
+        id: videoId,
+      },
+    }
+  );
+
+  if (!vLoading) {
+    if (dataFlag) {
+      editThumbnail.setValue(vData.getVideoOne.thumbnailPath);
+      editTitle.setValue(vData.getVideoOne.title);
+      editDesc.setValue(vData.getVideoOne.description);
+
+      setDataFlag(false);
+    }
+  }
   //////////// USE MUTATION  ////////////////
+
   ///////////// USE EFFECT  /////////////////
 
-  return <MM03Presenter />;
+  return (
+    <MM03Presenter
+      editThumbnail={editThumbnail}
+      editTitle={editTitle}
+      editDesc={editDesc}
+    />
+  );
 };
 
 export default MM03Container;
--- src/routers/MM03/MM03Queries.js
+++ src/routers/MM03/MM03Queries.js
@@ -1,1 +1,12 @@
 import { gql } from "apollo-boost";
+
+export const GET_VIDEO_ONE = gql`
+  query getVideoOne($id: String!) {
+    getVideoOne(id: $id) {
+      _id
+      thumbnailPath
+      title
+      description
+    }
+  }
+`;
